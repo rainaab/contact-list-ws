eafcc864b7e19be0b1540f88a0526d5cc22e5dbc
Raina Bethea <rainabethea@gmail.com>
2023-11-29 03:01:57 -0500

    fetch data from API

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,6 +1,7 @@
 import React from "react"
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ContactRow from "./ContactRow";
+
 
 const dummyContacts = [
     { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -12,6 +13,20 @@ const dummyContacts = [
 function ContactList (){
 
 const [contacts, setContacts]= useState(dummyContacts)
+
+useEffect(()=> {
+    async function fetchContacts(){
+        try{
+            const response = await fetch ("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users")
+            const result = await response.json()
+            setContacts(result)
+
+        } catch (error){
+            console.error(error)
+        }
+    }
+    fetchContacts()
+},[])
 
 console.log("Contacts: ", contacts)
 
